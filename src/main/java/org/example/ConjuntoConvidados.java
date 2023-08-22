a47744ea01e078ac65fc82fb7c8ffafa6f59e469
Marcio Carolino <marciocarolino@gmail.com>
2023-08-21 21:45:51 -0300

    feat: deixando a estrutura pronta

---
+++ src/main/java/org/example/ConjuntoConvidados.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    private Set<Convidado> convidadoSet;
+
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvit(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size(); // retornar a quantidade
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+}
---
+++ src/main/java/org/example/Convidado.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Convidado {
+
+    //Atributo
+    private String nome;
+    private int codigoConvite;
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+}
