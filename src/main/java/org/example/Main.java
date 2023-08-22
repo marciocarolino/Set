2a852f3b036c9178039867aae6985276edd6cb96
Marcio Carolino <marciocarolino@gmail.com>
2023-08-21 21:58:15 -0300

    primeiro teste com set funcionando

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1 ", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2 ", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3 ", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4 ", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvit(1234);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados");
+        conjuntoConvidados.exibirConvidados();
+
+
     }
 }
